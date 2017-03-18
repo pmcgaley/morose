d313749ff23cd499f071be7f3c1fec59ae46a59a
Paul Mc Galey <pmcgaley@Pauls-MacBook-Pro-3.local>
2017-03-18 15:59:56 +0000

    empty tabbed project created by xcode

---
+++ morose/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  morose
+//
+//  Created by Paul Mc Galey on 18/03/2017.
+//  Copyright © 2017 Macker. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
